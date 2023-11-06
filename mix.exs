5549cb573812761e058b620560d9d12479a5cf67
Nate Todd <ntodd@weigandconstruction.com>
2023-11-06 11:13:13 -0500

    Set version to 0.1.0 and lock deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MicrosoftGraph.Mixfile do
   def project do
     [
       app: :microsoft_graph,
-      version: "v1.0",
+      version: "0.1.0",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
